4348b8d6a8b36670dbfa2de81110cdc428f8201e
snamburi <reach.asquared@outlook.com>
2019-10-20 21:02:01 -0400

    my second java class

---
+++ src/test/java/asquared/MySecondJavaClass.java
@@ -0,0 +1,12 @@
+package asquared;
+
+import org.junit.Test;
+
+public class MySecondJavaClass {//class begins
+
+    @Test//annotation
+    public void mySecondMethod() {//method begins
+        System.out.println("Time to get out");
+
+    }//method ends
+}//class ends
