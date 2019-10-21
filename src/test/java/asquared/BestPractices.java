db2b81e90a094fde67bd2395fed0e56941f24377
snamburi <reach.asquared@outlook.com>
2019-10-20 20:10:49 -0400

    ide export

---
+++ src/test/java/asquared/BestPractices.java
@@ -0,0 +1,16 @@
+package asquared;
+
+import org.junit.Test;
+
+/**
+ * @project batch8
+ * Creation date: 16-04-2019
+ */
+public class BestPractices {//class begis
+
+    @Test
+    public void bestPractices() {//method begins
+
+
+    }//method ends
+}//class ends
---
+++ src/test/java/asquared/MyFirstJavaClass.java
@@ -0,0 +1,16 @@
+package asquared;
+
+import org.junit.Test;
+
+/**
+ * @project batch8
+ * Creation date: 15-04-2019
+ */
+public class MyFirstJavaClass {
+
+    @Test
+    public void myMethod() {
+        System.out.println("Hello ASquared");
+    }
+
+}
