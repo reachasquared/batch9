35eace1500e5fde35d6a4a217effe87a2b3f8257
snamburi <reach.asquared@outlook.com>
2019-10-22 19:38:37 -0400

    Library

---
+++ src/main/java/library/TestLocations.java
@@ -0,0 +1,14 @@
+package library;
+
+public class TestLocations {
+    /**
+     * Display "I see"
+     */
+    public void jacksonvilleLocation() {//method iSee begins
+        System.out.print("In Prachee Arts, Jacksonville Location, I see ");
+    }//method iSee ends
+
+    public void stJohnsLocation() {//method iSeeStJohns begins
+        System.out.print("In Prachee Arts, St Johns Location, I see ");
+    }// iSeeStJohns method ends
+}
