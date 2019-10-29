6d3e6a52ae80aa942c254a3679e4335e6051bd82
venkat <narayanraob@gmail.com>
2019-10-29 19:48:41 -0400

    firstjava to commit
    TEST

---
+++ src/test/java/narayana/MyFirstJavaClass.java
@@ -0,0 +1,21 @@
+package narayana;
+
+import org.junit.Test;
+
+//class   java starts with class
+public class MyFirstJavaClass {//class begins
+    //method   In java after class it starts with method with () and open { and close will automatically appear
+    //@Test for executable (give before method.)
+    @Test//Annotation
+    public void myFirstMethod(){//method begins
+        System.out.println("This is my first java program");
+
+
+
+
+    }//method ends
+
+
+
+
+}//class ends
