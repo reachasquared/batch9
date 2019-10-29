8dbbe5231c2ac981ee6820443e165ef284b6479c
venkat <narayanraob@gmail.com>
2019-10-29 19:20:33 -0400

    Locations.java  to commit
    TEST

---
+++ src/test/java/narayana/Locations.java
@@ -0,0 +1,26 @@
+package narayana;
+
+import org.junit.Test;
+
+public class Locations {
+
+    @Test
+    public void jacksonville(){
+        System.out.println("Iron safe   ");
+        stJohns();
+        System.out.println(" Reception table   ");
+        orangePark();
+        System.out.println(" Chairs    ");
+    }
+
+    public void stJohns(){
+
+        System.out.println("In St Johns Location, I see Reception table   ");
+
+    }
+
+    public void orangePark(){
+        System.out.println("In Orange Park Location, I see Chairs    ");
+
+    }
+}
