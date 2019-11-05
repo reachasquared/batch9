e9b5420e2f563227e3edb913cccb8d67795c2231
rajyalakshmi <svnrlakshmi@gmail.com>
2019-11-04 20:04:34 -0500

    my first commit

---
+++ src/test/java/lakshmi/TestRegistration.java
@@ -0,0 +1,38 @@
+package lakshmi;
+
+import org.junit.Test;
+
+public class TestRegistration {//class begins
+
+    @Test
+    public void registration() {//method registration begins
+        //Verify REGISTER link is present using WebDriver linktext method
+
+        //Click on REGISTER link using xpath
+
+        //Enter asquared in First Name - using xpath @name
+
+        //Enter asquared in Last Name - using following-sibling by label
+
+        //Enter 9049049044 in Phone - using descendant by label
+
+        //Select UNITED KINGDOM from Country
+
+        //Click Submit
+
+        //Verify "Dear asquare asquared," text is displayed
+
+        //verify Sign-off link exists
+
+        // Verify REGISTER image is displayed
+
+        //locate username and enterdata into the textbox
+
+
+        //click SIGN-OFF
+
+
+    }//method registration ends
+
+
+}//class ends
